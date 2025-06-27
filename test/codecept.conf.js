59a8f6d3646d5733773d6d7bbb77d9cc1182969c
GhostyAD-TranThanhTrung <tranthanhtrung2015@gmail.com>
2025-06-27 16:39:41 +0700

    update

--- test/codecept.conf.js
+++ test/codecept.conf.js
@@ -4,12 +4,27 @@ exports.config = {
   output: './output',
   helpers: {
     REST: {
-      endpoint: 'http://localhost:3000/api'
+      endpoint: 'http://localhost:3000/api',
+      timeout: 10000,
+      defaultHeaders: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
     },
     JSONResponse: {}
   },
   include: {
     I: './steps_file.js'
   },
-  name: 'test'
+  bootstrap: null,
+  mocha: {},
+  name: 'test',
+  plugins: {
+    retryFailedStep: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: false
+    }
+  }
 }
